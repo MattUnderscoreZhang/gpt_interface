2b1ac25fd3ccb501b958cb81adaf5cd0ef6d6771
Matt Zhang <set.stun@gmail.com>
2023-11-15 15:42:24 -0600

    Added another example for how to save and load logs

---
+++ examples/log_saving.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+from typing import cast
+
+from gpt_interface import GptInterface
+
+
+def save_log():
+    interface = GptInterface(
+        openai_api_key=cast(str, os.getenv("OPENAI_API_KEY")),
+        model="gpt-4",
+    )
+    interface.say("Hi there!")
+    my_path = Path(__file__).parent
+    interface.log.save(my_path / "my_log.json")
+
+
+def load_log():
+    interface = GptInterface(
+        openai_api_key=cast(str, os.getenv("OPENAI_API_KEY")),
+        model="gpt-4",
+    )
+    my_path = Path(__file__).parent
+    interface.log.load(my_path / "my_log.json")
+    print(interface.log)
+
+
+if __name__ == "__main__":
+    load_dotenv()  # load the OpenAI API key from a .env file
+    save_log()
+    load_log()
