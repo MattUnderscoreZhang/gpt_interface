43997c23f8807d33911c81a3d23dfbd99c9e47cf
Matt Zhang <set.stun@gmail.com>
2024-05-14 02:10:45 -0400

    Added simple conversation script

---
+++ examples/talk.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+from termcolor import cprint
+from typing import cast
+
+from gpt_interface import GptInterface
+
+
+if __name__ == "__main__":
+    load_dotenv()  # load the OpenAI API key from a .env file
+    model = "gpt-4o"
+    interface = GptInterface(
+        api_key=cast(str, os.getenv("OPENAI_API_KEY")),
+        model=model,
+        json_mode=True,
+    )
+    cprint(f"You are talking to {model}.\n", "blue")
+    while True:
+        response = interface.say(input())
+        cprint(f"\n{response}\n\n", "blue")
